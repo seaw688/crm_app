84a84bf8612daaff0c026aa8412e303b6126522d
Vlad Honcharov <snork98@gmail.com>
2019-03-07 15:22:01 +0200

    added login form and view

--- CRM_APP/USER/views.py
+++ CRM_APP/USER/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
 
 class LoginView(View):
-    def get(self, request):
-        return render(request,'login.html')
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            pass
+            #return redirect()
+        form  = AuthenticationForm
+        return render(request,'login.html',context={'form':form})
+
+    def post(self,request, *args, **kwargs):
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return HttpResponse('ok')
+        return render(request, 'login.html', context={'form': form})
