a40b85a8dd523a4768fcf06f42b8a5b46c0f8ebc
Vlad Honcharov <snork98@gmail.com>
2019-03-12 18:50:35 +0200

    update

--- CRM_APP/USER/admin.py
+++ CRM_APP/USER/admin.py
@@ -4,9 +4,16 @@ from  django.contrib.auth import get_user_model
 
 UserModel = get_user_model()
 
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = UserAdmin.fieldsets + (
+#         ('Personal info', {'fields': ('birth','avatar','position')}),
+#     )
+#
+#     model = UserModel
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('birth',)}),
+        ('Personal info', {'fields': ('birth','avatar','position')}),
     )
 
     model = UserModel
--- CRM_APP/USER/models.py
+++ CRM_APP/USER/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from CRM_APP import settings
+
+
+def user_avatar_upload_path(instance, filename):
+    return str(settings.MEDIA_ROOT + '/users/{0}/avatar/{1}'.format(instance.username, filename))
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     birth = models.DateTimeField(null=True)
+    avatar = models.ImageField(upload_to=user_avatar_upload_path,blank=True,null=True)
+    position = models.CharField(max_length=30,null=True,blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username',]
