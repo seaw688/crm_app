6d4ea685d26ea2e71d266cc619d8b5e83af8ef6f
seaw688 <snork98@gmail.com>
2019-03-18 23:37:15 +0300

    upd

--- CRM_APP/MAIN/admin.py
+++ CRM_APP/MAIN/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from MAIN.models import Project, Task, TaskComment, TimeLog
+from MAIN.models import Project, Task, TaskComment, TimeLog ,TaskFile
 from django.contrib.auth.models import Permission
+
+
+class TaskFileInline(admin.TabularInline):
+    model = TaskFile
+    extra = 2
+
+class TaskAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [TaskFileInline, ]
+
+
 
 admin.site.register(Permission)
 admin.site.register(Project)
-admin.site.register(Task)
+admin.site.register(Task,TaskAdmin)
 admin.site.register(TaskComment)
 admin.site.register(TimeLog)
